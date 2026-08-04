5b93616cc28ac3c7552d16c2ca686262b1e7093b
sayalij1609 <jadhavsayali489@gmail.com>
2026-08-04 20:39:03 +0530

    Updated config file

--- config.py
+++ config.py
@@ -29,6 +29,14 @@ class Config:
         "uploads"
     )
     
-
     # Maximum upload size (16 MB)
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+
+    MAIL_DEFAULT_SENDER = MAIL_USERNAME
